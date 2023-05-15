638e850048fb53c0e5356ec06398414f88d9b503
Infinite Aengus <infiniteaengus@gmail.com>
2023-05-15 17:35:50 -0400

    removed unused modules

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,48 +1,55 @@
-import { useWallet } from "@suiet/wallet-kit";
+import { useWallet } from '@suiet/wallet-kit';
 import { devnetConnection, JsonRpcProvider } from '@mysten/sui.js';
-import { useState, useEffect } from "react"
+import { useState, useEffect } from 'react';
 
 const Header = () => {
   const wallet = useWallet();
-  const [balance, setBalance] = useState<string>("");
+  const [balance, setBalance] = useState<string>('');
 
   useEffect(() => {
     (async () => {
       if (!wallet.address) return;
       const provider = new JsonRpcProvider(devnetConnection);
-      setBalance(Math.round((await provider.getBalance({
-        owner: wallet.address || ""
-      })).totalBalance / 1e7) / 100);
+      const providerBalance = await provider.getBalance({
+        owner: wallet.address || '',
+      });
+
+      const accountBalance: any = Math.round(providerBalance.totalBalance / 1e7) / 100;
+
+      setBalance(accountBalance.toString());
     })();
   }, [wallet]);
+
   const disconnect = () => {
     wallet.disconnect();
-    window.location.href = "/";
-  }
+    window.location.href = '/';
+  };
 
   return (
-    <div className="px-[145px] pt-[90px] flex justify-between">
-      <div className="flex gap-6 h-fit">
-        <img src="/images/speaker.png" className="w-[63px] h-[53px]" />
-        <button className="bg-primary hover:bg-white anim border-2 border-black rounded-md px-8 font-semibold text-xl">BETR</button>
+    <div className='px-[145px] pt-[90px] flex justify-between'>
+      <div className='flex gap-6 h-fit'>
+        <img src='/images/speaker.png' className='w-[63px] h-[53px]' />
+        <button className='bg-primary hover:bg-white anim border-2 border-black rounded-md px-8 font-semibold text-xl'>
+          BETR
+        </button>
       </div>
 
-      <div className="flex gap-5 font-semibold text-xl">
-        <div className="px-5 py-3 flex gap-1 items-center bg-white hover:bg-primary anim h-fit rounded-md cursor-pointer">
+      <div className='flex gap-5 font-semibold text-xl'>
+        <div className='px-5 py-3 flex gap-1 items-center bg-white hover:bg-primary anim h-fit rounded-md cursor-pointer'>
           RECENT
-          <img src="/images/down-arrow.png" className="w-auto h-fit" />
+          <img src='/images/down-arrow.png' className='w-auto h-fit' />
         </div>
-        <div className="px-5 py-3 flex gap-1 items-center bg-white hover:bg-primary anim h-fit rounded-md cursor-pointer">
+        <div className='px-5 py-3 flex gap-1 items-center bg-white hover:bg-primary anim h-fit rounded-md cursor-pointer'>
           LEADERBOARD
-          <img src="/images/down-arrow.png" className="w-fit h-fit" />
+          <img src='/images/down-arrow.png' className='w-fit h-fit' />
         </div>
-        <div className="flex flex-col items-center -mt-3">
-          <img src="/images/small-icon.png" className="" onClick={disconnect}/>
+        <div className='flex flex-col items-center -mt-3'>
+          <img src='/images/small-icon.png' className='' onClick={disconnect} />
           <span>{`${balance ?? 100} sui`}</span>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
