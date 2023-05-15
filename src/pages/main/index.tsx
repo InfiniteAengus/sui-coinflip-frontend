06fcac741fa6850c133c4cf4eb90360922528e97
InfiniteAengus <InfiniteAengus@gmail.com>
2023-05-15 11:34:46 -0400

    updated connect button and balance

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,4 +1,3 @@
-import Button from "@/components/Button"
 import HistoryItem from "@/components/HistoryItem"
 import { ConnectButton, useWallet } from '@suiet/wallet-kit';
 import { useEffect } from "react";
@@ -38,13 +37,18 @@ const Main = () => {
     window.location.href = "/play";
   }, [wallet.connected])
 
+
   return (
     <div className="relative w-full flex flex-col items-center justify-center pb-20">
       <span className="text-4xl">sui or nothing?</span>
       <img src="/images/smile-coin.png" className="w-[300px] mt-10" />
 
       <div className="mt-10">
-        <Button label="connect wallet" className="!rounded-[6px] !text-3xl" />
+        <ConnectButton className='pushable !rounded-[6px] !w-[unset] !leading-[unset] !font-[Symtext] !font-normal'>
+          <span className='front !text-3xl !rounded-[6px]'>
+            connect wallet
+          </span>
+        </ConnectButton>
       </div>
 
       <div className="mt-10 flex flex-col items-center">
@@ -55,7 +59,7 @@ const Main = () => {
           ))}
         </div>
       </div>
-      <ConnectButton />
+
     </div>
   )
 }
