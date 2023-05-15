65e283a5be39c03c074759c0a3397815a5403ab8
InfiniteAengus <InfiniteAengus@gmail.com>
2023-05-15 17:35:50 -0400

    removed unused modules

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,67 +1,69 @@
-import HistoryItem from "@/components/HistoryItem"
+import HistoryItem from '@/components/HistoryItem';
 import { ConnectButton, useWallet } from '@suiet/wallet-kit';
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
 const dummyData = [
   {
-    mainContent: "PLAYER NAME FLIPPED 1.0 SUI AND",
-    subContent: "DOUBLE 2 TIMES",
-    time: "3 MINUTES AGO",
-    win: true
+    mainContent: 'PLAYER NAME FLIPPED 1.0 SUI AND',
+    subContent: 'DOUBLE 2 TIMES',
+    time: '3 MINUTES AGO',
+    win: true,
   },
   {
-    mainContent: "PLAYER NAME FLIPPED 1.0 SUI AND",
-    subContent: "DOUBLE 2 TIMES",
-    time: "3 MINUTES AGO",
-    win: true
+    mainContent: 'PLAYER NAME FLIPPED 1.0 SUI AND',
+    subContent: 'DOUBLE 2 TIMES',
+    time: '3 MINUTES AGO',
+    win: true,
   },
   {
-    mainContent: "PLAYER NAME FLIPPED 1.0 SUI AND",
-    subContent: "DOUBLE 2 TIMES",
-    time: "3 MINUTES AGO",
-    win: false
+    mainContent: 'PLAYER NAME FLIPPED 1.0 SUI AND',
+    subContent: 'DOUBLE 2 TIMES',
+    time: '3 MINUTES AGO',
+    win: false,
   },
   {
-    mainContent: "PLAYER NAME FLIPPED 1.0 SUI AND",
-    subContent: "DOUBLE 2 TIMES",
-    time: "3 MINUTES AGO",
-    win: true
+    mainContent: 'PLAYER NAME FLIPPED 1.0 SUI AND',
+    subContent: 'DOUBLE 2 TIMES',
+    time: '3 MINUTES AGO',
+    win: true,
   },
-]
+];
 
 const Main = () => {
-  const wallet = useWallet()
+  const wallet = useWallet();
 
   useEffect(() => {
     if (!wallet.connected) return;
-    window.location.href = "/play";
-  }, [wallet.connected])
-
+    window.location.href = '/play';
+  }, [wallet.connected]);
 
   return (
-    <div className="relative w-full flex flex-col items-center justify-center pb-20">
-      <span className="text-4xl">sui or nothing?</span>
-      <img src="/images/smile-coin.png" className="w-[300px] mt-10" />
+    <div className='relative w-full flex flex-col items-center justify-center pb-20'>
+      <span className='text-4xl'>sui or nothing?</span>
+      <img src='/images/smile-coin.png' className='w-[300px] mt-10' />
 
-      <div className="mt-10">
+      <div className='mt-10'>
         <ConnectButton className='pushable !rounded-[6px] !w-[unset] !leading-[unset] !font-[Symtext] !font-normal'>
-          <span className='front !text-3xl !rounded-[6px]'>
-            connect wallet
-          </span>
+          <span className='front !text-3xl !rounded-[6px]'>connect wallet</span>
         </ConnectButton>
       </div>
 
-      <div className="mt-10 flex flex-col items-center">
-        <span className="text-2xl">RECENT PLAYERS</span>
-        <div className="flex flex-col gap-4 mt-5">
+      <div className='mt-10 flex flex-col items-center'>
+        <span className='text-2xl'>RECENT PLAYERS</span>
+        <div className='flex flex-col gap-4 mt-5'>
           {dummyData.map((item: any, id: number) => (
-            <HistoryItem mainContent={item.mainContent} subContent={item.subContent} time={item.time} win={item.win} key={id} />
+            <HistoryItem
+              mainContent={item.mainContent}
+              subContent={item.subContent}
+              time={item.time}
+              win={item.win}
+              key={id}
+            />
           ))}
         </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
--- src/pages/play/index.tsx
+++ src/pages/play/index.tsx
@@ -1,72 +1,73 @@
-import Button from "@/components/Button"
+import Button from '@/components/Button';
 
-import { useState, useEffect } from "react"
+import { useState, useEffect } from 'react';
 import Confetti from 'react-confetti';
 import { ToastContainer, toast } from 'react-toastify';
 
 import { TransactionBlock, devnetConnection, JsonRpcProvider } from '@mysten/sui.js';
 import { useWallet } from '@suiet/wallet-kit';
-import { bytesToHex, randomBytes } from "@noble/hashes/utils";
+import { bytesToHex, randomBytes } from '@noble/hashes/utils';
 import { useWindowSize } from '@react-hook/window-size';
 
 import 'react-toastify/dist/ReactToastify.css';
 
-const statusList = ['init', 'deposit', 'flipping', 'won', 'lost']
+const statusList = ['init', 'deposit', 'flipping', 'won', 'lost'];
 
 const Play = () => {
   const [width, height] = useWindowSize();
-  const [status, setStatus] = useState<string>("init");
-  const [guess, setGuess] = useState<string>("");
+  const [status, setStatus] = useState<string>('init');
+  const [guess, setGuess] = useState<string>('');
   const [betAmount, setBetAmount] = useState<number>(0);
-  const [previousBalance, setPreviousBalance] = useState<string>("0");
+  const [previousBalance, setPreviousBalance] = useState<string>('0');
   const wallet = useWallet();
 
   const nextStatus = () => {
-    setStatus(statusList[(statusList.indexOf(status) + 1) % statusList.length] ?? "init");
-  }
+    setStatus(statusList[(statusList.indexOf(status) + 1) % statusList.length] ?? 'init');
+  };
 
   useEffect(() => {
     const getBalance = async () => {
       const provider = new JsonRpcProvider(devnetConnection);
-      return (await provider.getBalance({
-        owner: wallet.address || ""
-      })).totalBalance;
-    }
+      return (
+        await provider.getBalance({
+          owner: wallet.address || '',
+        })
+      ).totalBalance;
+    };
 
     (async () => {
-      if (status == "deposit") {
+      if (status == 'deposit') {
         setTimeout(() => {
-          setStatus("flipping");
+          setStatus('flipping');
         }, 1000);
       }
-      if (status == "flipping") {
-        await new Promise(r => setTimeout(r, 1000));
+      if (status == 'flipping') {
+        await new Promise((r) => setTimeout(r, 1000));
         let balance = await getBalance();
         if (Number(balance) < Number(previousBalance)) {
-          setStatus("lost");
+          setStatus('lost');
           toast.error('You lost :(', {
-            position: "top-right",
+            position: 'top-right',
             autoClose: 2000,
             hideProgressBar: false,
             closeOnClick: true,
             pauseOnHover: true,
             draggable: true,
             progress: undefined,
-            theme: "colored",
+            theme: 'colored',
           });
         } else {
-          setStatus("won");
+          setStatus('won');
           toast.success('You won :)', {
-            position: "top-right",
+            position: 'top-right',
             autoClose: 2000,
             hideProgressBar: false,
             closeOnClick: true,
             pauseOnHover: true,
             draggable: true,
             progress: undefined,
-            theme: "colored",
+            theme: 'colored',
           });
-
         }
       }
     })();
@@ -82,11 +83,11 @@ const Play = () => {
         showInput: false,
         showEvents: false,
         showObjectChanges: false,
-        showBalanceChanges: false
-      }
+        showBalanceChanges: false,
+      },
     });
     let data = await provider.getObject({
-      id: "0xe30dfe21e6a70d93d7362c05ab5c00a00a70b270777e3abf75ca45be87b0e6fc",
+      id: '0xe30dfe21e6a70d93d7362c05ab5c00a00a70b270777e3abf75ca45be87b0e6fc',
       options: { showContent: true },
     });
     console.log(data);
@@ -105,18 +106,26 @@ const Play = () => {
 
     const coins = txb.splitCoins(txb.gas, [txb.pure(betAmount * 1000000000)]);
 
-    const packageObjectId = "0xcd43c69d5ad829f979a89e9394c5831445e2090bda3a9374ad80361f3576ab4a";
-    const houseDataId = "0xe252026aa62a5e91aeb769dad9266a9cdc53288594f774d00a89cd451a6ad170";
+    const packageObjectId = '0xcd43c69d5ad829f979a89e9394c5831445e2090bda3a9374ad80361f3576ab4a';
+    const houseDataId = '0xe252026aa62a5e91aeb769dad9266a9cdc53288594f774d00a89cd451a6ad170';
     txb.moveCall({
       target: `${packageObjectId}::coin_flip::play`,
-      arguments: [txb.pure(guess === "head" ? 1 : 0), txb.pure(ser), coins[0] as any, txb.pure(betAmount * 1000000000), txb.pure(houseDataId)],
+      arguments: [
+        txb.pure(guess === 'head' ? 1 : 0),
+        txb.pure(ser),
+        coins[0] as any,
+        txb.pure(betAmount * 1000000000),
+        txb.pure(houseDataId),
+      ],
     });
     txb.setGasBudget(50000000);
     const getBalance = async () => {
-      return (await provider.getBalance({
-        owner: wallet.address || ""
-      })).totalBalance;
-    }
+      return (
+        await provider.getBalance({
+          owner: wallet.address || '',
+        })
+      ).totalBalance;
+    };
     try {
       let prvBalance = await getBalance();
       setPreviousBalance(prvBalance);
@@ -127,90 +136,149 @@ const Play = () => {
     } catch (e) {
       console.log(e);
     }
-  }
+  };
 
   return (
     <>
-      {status === "init" && <div className="w-full flex flex-col items-center justify-center pb-20">
-        <img src="/images/smile-coin.png" className="w-[300px]" />
+      {status === 'init' && (
+        <div className='w-full flex flex-col items-center justify-center pb-20'>
+          <img src='/images/smile-coin.png' className='w-[300px]' />
 
-        <div className="mt-10 flex flex-col items-center">
-          <div className="flex gap-10">
-            <Button label="heads" className="!rounded-[18px] !text-3xl" onClick={() => setGuess("head")} selected={guess === "head"} />
-            <Button label="tails" className="!rounded-[18px] !text-3xl" onClick={() => setGuess("tail")} selected={guess === "tail"} />
-          </div>
+          <div className='mt-10 flex flex-col items-center'>
+            <div className='flex gap-10'>
+              <Button
+                label='heads'
+                className='!rounded-[18px] !text-3xl'
+                onClick={() => setGuess('head')}
+                selected={guess === 'head'}
+              />
+              <Button
+                label='tails'
+                className='!rounded-[18px] !text-3xl'
+                onClick={() => setGuess('tail')}
+                selected={guess === 'tail'}
+              />
+            </div>
 
-          <span className="text-2xl py-4">for</span>
+            <span className='text-2xl py-4'>for</span>
 
-          <div className="grid grid-cols-3 gap-6">
-            <Button label="1 sui" className="!rounded-[18px] !text-3xl" onClick={() => setBetAmount(1)} selected={betAmount === 1} />
-            <Button label="2 sui" className="!rounded-[18px] !text-3xl" onClick={() => setBetAmount(2)} selected={betAmount === 2} />
-            <Button label="5 sui" className="!rounded-[18px] !text-3xl" onClick={() => setBetAmount(5)} selected={betAmount === 5} />
-            <Button label="10 sui" className="!rounded-[18px] !text-3xl" onClick={() => setBetAmount(10)} selected={betAmount === 10} />
-            <Button label="25 sui" className="!rounded-[18px] !text-3xl" onClick={() => setBetAmount(25)} selected={betAmount === 25} />
-            <Button label="50 sui" className="!rounded-[18px] !text-3xl" onClick={() => setBetAmount(50)} selected={betAmount === 50} />
-          </div>
+            <div className='grid grid-cols-3 gap-6'>
+              <Button
+                label='1 sui'
+                className='!rounded-[18px] !text-3xl'
+                onClick={() => setBetAmount(1)}
+                selected={betAmount === 1}
+              />
+              <Button
+                label='2 sui'
+                className='!rounded-[18px] !text-3xl'
+                onClick={() => setBetAmount(2)}
+                selected={betAmount === 2}
+              />
+              <Button
+                label='5 sui'
+                className='!rounded-[18px] !text-3xl'
+                onClick={() => setBetAmount(5)}
+                selected={betAmount === 5}
+              />
+              <Button
+                label='10 sui'
+                className='!rounded-[18px] !text-3xl'
+                onClick={() => setBetAmount(10)}
+                selected={betAmount === 10}
+              />
+              <Button
+                label='25 sui'
+                className='!rounded-[18px] !text-3xl'
+                onClick={() => setBetAmount(25)}
+                selected={betAmount === 25}
+              />
+              <Button
+                label='50 sui'
+                className='!rounded-[18px] !text-3xl'
+                onClick={() => setBetAmount(50)}
+                selected={betAmount === 50}
+              />
+            </div>
 
-          <div className="mt-8">
-            <Button label="play" className="!rounded-[40px] !text-4xl !bg-[#1D4F88]" extraClassName="!bg-[#091B2E]" onClick={playGame} />
-          </div>
+            <div className='mt-8'>
+              <Button
+                label='play'
+                className='!rounded-[40px] !text-4xl !bg-[#1D4F88]'
+                extraClassName='!bg-[#091B2E]'
+                onClick={playGame}
+              />
+            </div>
 
-          <div className="mt-20 flex gap-1">
-            <span>faq</span>
-            <span>|</span>
-            <span>how to play</span>
-            <span>|</span>
-            <span>Flip responsibly</span>
-          </div>
-        </div>
-      </div>}
-
-      {status === "deposit" && (
-        <div className="w-full flex flex-col items-center justify-center pb-20">
-          <img src="/images/smile-coin.png" className="w-[300px]" />
-          <p className="mt-10 text-2xl">waiting for deposit....</p>
-          <p className="text-base">heads for 1.0 sui</p>
-        </div>
-      )}
-
-      {status === "flipping" && (
-        <div className="w-full flex flex-col items-center justify-center pb-20">
-          <img src="/images/smile-coin.png" className="w-[300px]" />
-          <p className="mt-10 text-2xl">flipping..</p>
-          <p className="text-base">heads for 2.0 sui</p>
-        </div>
-      )}
-
-      {status === "won" && (
-        <>
-          <div className="w-full flex flex-col items-center justify-center pb-20">
-            <img src="/images/smile-coin.png" className="w-[300px]" />
-            <p className="mt-10 text-4xl">you won!</p>
-            <div className="text-xl bg-black py-2 px-8 text-[#53DD82] mt-1 rounded-md">2.0 sui</div>
-            <div className="mt-5">
-              <Button label="try again?" className="!rounded-[6px] !text-xl" onClick={() => { setStatus("init"); setGuess(""); setBetAmount(0); }} />
+            <div className='mt-20 flex gap-1'>
+              <span>faq</span>
+              <span>|</span>
+              <span>how to play</span>
+              <span>|</span>
+              <span>Flip responsibly</span>
             </div>
           </div>
-          <Confetti
-            width={width ? width : 1920}
-            height={height ? height : 1500}
-            recycle={false}
-          />
+        </div>
+      )}
+
+      {status === 'deposit' && (
+        <div className='w-full flex flex-col items-center justify-center pb-20'>
+          <img src='/images/smile-coin.png' className='w-[300px]' />
+          <p className='mt-10 text-2xl'>waiting for deposit....</p>
+          <p className='text-base'>heads for 1.0 sui</p>
+        </div>
+      )}
+
+      {status === 'flipping' && (
+        <div className='w-full flex flex-col items-center justify-center pb-20'>
+          <img src='/images/smile-coin.png' className='w-[300px]' />
+          <p className='mt-10 text-2xl'>flipping..</p>
+          <p className='text-base'>heads for 2.0 sui</p>
+        </div>
+      )}
+
+      {status === 'won' && (
+        <>
+          <div className='w-full flex flex-col items-center justify-center pb-20'>
+            <img src='/images/smile-coin.png' className='w-[300px]' />
+            <p className='mt-10 text-4xl'>you won!</p>
+            <div className='text-xl bg-black py-2 px-8 text-[#53DD82] mt-1 rounded-md'>2.0 sui</div>
+            <div className='mt-5'>
+              <Button
+                label='try again?'
+                className='!rounded-[6px] !text-xl'
+                onClick={() => {
+                  setStatus('init');
+                  setGuess('');
+                  setBetAmount(0);
+                }}
+              />
+            </div>
+          </div>
+          <Confetti width={width ? width : 1920} height={height ? height : 1500} recycle={false} />
         </>
       )}
 
-      {status === "lost" && (
-        <div className="w-full flex flex-col items-center justify-center pb-20">
-          <img src="/images/cry-icon.png" className="w-[300px]" />
-          <p className="mt-10 text-4xl">you lost!</p>
-          <div className="text-xl bg-black py-2 px-8 text-[#F02323] mt-1 rounded-md">2.0 sui</div>
-          <div className="mt-5">
-            <Button label="try again?" className="!rounded-[6px] !text-xl" onClick={() => { setStatus("init"); setGuess(""); setBetAmount(0); }} />
+      {status === 'lost' && (
+        <div className='w-full flex flex-col items-center justify-center pb-20'>
+          <img src='/images/cry-icon.png' className='w-[300px]' />
+          <p className='mt-10 text-4xl'>you lost!</p>
+          <div className='text-xl bg-black py-2 px-8 text-[#F02323] mt-1 rounded-md'>2.0 sui</div>
+          <div className='mt-5'>
+            <Button
+              label='try again?'
+              className='!rounded-[6px] !text-xl'
+              onClick={() => {
+                setStatus('init');
+                setGuess('');
+                setBetAmount(0);
+              }}
+            />
           </div>
         </div>
       )}
       <ToastContainer
-        position="top-right"
+        position='top-right'
         autoClose={5000}
         hideProgressBar={false}
         newestOnTop={false}
@@ -219,10 +287,10 @@ const Play = () => {
         pauseOnFocusLoss
         draggable
         pauseOnHover
-        theme="colored"
+        theme='colored'
       />
     </>
-  )
-}
+  );
+};
 
-export default Play
+export default Play;
