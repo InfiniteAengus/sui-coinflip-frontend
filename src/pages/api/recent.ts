99105c4f5b453ca06c18244304a0d21861c5a18d
InfiniteAengus <InfiniteAengus@gmail.com>
2023-05-16 11:49:28 -0400

    added menu

--- src/pages/api/recent.ts
+++ src/pages/api/recent.ts
@@ -2,5 +2,6 @@ import { NextApiResponse, NextApiRequest } from 'next';
 import { getRecent } from '../../controllers/data';
 
 export default function handler(_req: NextApiRequest, res: NextApiResponse) {
+  console.log('log');
   return res.status(200).json(getRecent());
 }
