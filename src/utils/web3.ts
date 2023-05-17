dcddb0c667a7d52e4687a1099ddda1d5a1e7ee0b
Infinite Aengus <infiniteaengus@gmail.com>
2023-05-17 08:44:43 -0400

    updated the code

--- src/utils/web3.ts
+++ src/utils/web3.ts
@@ -3,14 +3,14 @@ import { PlayResult } from '@/utils/types';
 
 export const getPlayResultFromTx = async (digestId: string) => {
   if (!digestId) {
-    console.error("Invalid digestId");
+    console.error('Invalid digestId');
   }
 
   let result: PlayResult = {
     timestamp: 0,
     won: false,
     address: '',
-    betAmount: 0
+    betAmount: 0,
   };
 
   const provider = new JsonRpcProvider(devnetConnection);
@@ -32,14 +32,14 @@ export const getPlayResultFromTx = async (digestId: string) => {
       options: { showContent: true },
     });
     if (!objectContents.error) {
-      let content = objectContents.data?.content;
+      let content: any = objectContents.data?.content;
       if (content?.type.includes('Outcome')) {
         result.won = content.fields?.player_won;
         result.outcomeId = object.reference.objectId;
       }
       if (content?.type.includes('Game')) {
         result.gameId = object.reference.objectId;
-        result.betAmount = content.fields.stake_amount;
+        result.betAmount = content.fields.stake_amount / 10 ** 9;
         result.address = content.fields.player;
         result.timestamp = Date.now();
       }
