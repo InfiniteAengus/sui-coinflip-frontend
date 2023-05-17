cfd9233bc7038806734542bcd85438f20a90874f
InfiniteAengus <InfiniteAengus@gmail.com>
2023-05-17 10:47:02 -0400

    updated design matching the screen size

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,13 +1,17 @@
 const Footer = () => {
   return (
-    <div className='flex gap-5 px-[145px] pb-14'>
-      <a href='https://discord.gg/vdgXN7XZmG' target='_blank' rel='noreferrer'>
-        <img src='/images/discord.png' className='h-7 w-auto cursor-pointer' />
-      </a>
-      <a href='https://twitter.com/DeSuiFlip' target='_blank' rel='noreferrer'>
-        <img src='/images/twitter.png' className='h-7 w-auto cursor-pointer' />
-      </a>
-    </div>
+    <footer style={{ zoom: '0.7' }}>
+      <div className='container pb-10'>
+        <div className='mt-auto flex gap-5'>
+          <a href='https://discord.gg/vdgXN7XZmG' target='_blank' rel='noreferrer'>
+            <img src='/images/discord.png' className='h-7 w-auto cursor-pointer' />
+          </a>
+          <a href='https://twitter.com/DeSuiFlip' target='_blank' rel='noreferrer'>
+            <img src='/images/twitter.png' className='h-7 w-auto cursor-pointer' />
+          </a>
+        </div>
+      </div>
+    </footer>
   );
 };
 
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -9,7 +9,9 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <div className='flex min-h-screen w-full flex-col'>
       <Header />
-      <main className='flex w-full'>{children}</main>
+      <main className='flex w-full flex-grow items-center' style={{ zoom: '0.5' }}>
+        {children}
+      </main>
       <Footer />
     </div>
   );
