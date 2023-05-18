685668cc462347e36f1418f77fbbecb24c961aea
InfiniteAengus <InfiniteAengus@gmail.com>
2023-05-17 21:05:54 -0400

    pretty the code

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,6 +1,6 @@
-export const PACKAGE_ID = "0x966036476538776644d6ed815e6adc140226bd4e161ef1f267e7663c2e1239b9";
-export const HOUSE_DATA_ID = "0x5b74621fde91da9d2e8a887943fd11912eafbcd9f91adc1df57abf81174bbf69";
+export const PACKAGE_ID = '0x966036476538776644d6ed815e6adc140226bd4e161ef1f267e7663c2e1239b9';
+export const HOUSE_DATA_ID = '0x5b74621fde91da9d2e8a887943fd11912eafbcd9f91adc1df57abf81174bbf69';
 
-export const HOLDER_REVENUE_WALLET = "";
-export const SMART_CONTRACT_WALLET = "";
-export const PROJECT_DEVELOPMENT_WALLET = "";
+export const HOLDER_REVENUE_WALLET = '';
+export const SMART_CONTRACT_WALLET = '';
+export const PROJECT_DEVELOPMENT_WALLET = '';
