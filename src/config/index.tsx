f73ea34e438ff505c9e1a3213b9e92050169d002
Infinite Aengus <infiniteaengus@gmail.com>
2023-05-20 12:23:38 -0400

    updated config

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,14 +1,14 @@
-export const mode = "dev";
+export const mode = process.env.NEXT_PUBLIC_MODE;
 // devnet
-const DEV_PACKAGE_ID = "0x966036476538776644d6ed815e6adc140226bd4e161ef1f267e7663c2e1239b9";
-const DEV_HOUSE_DATA_ID = "0x5b74621fde91da9d2e8a887943fd11912eafbcd9f91adc1df57abf81174bbf69";
+const DEV_PACKAGE_ID = process.env.NEXT_PUBLIC_DEV_PACKAGE_ID;
+const DEV_HOUSE_DATA_ID = process.env.NEXT_PUBLIC_DEV_HOUSE_DATA_ID;
 
 // // mainnet
-const PRODUCT_PACKAGE_ID = "0xdc16f545b224e66f12f2ac52688a8927209bf9640234abf91d47fd8ef8ff3ded";
-const PRODUCT_HOUSE_DATA_ID = "0x72eaacc50b0158254bf44c38dac9200ab51de43eeed42a8fae2b3d65a2ac9fa3";
+const PRODUCT_PACKAGE_ID = process.env.NEXT_PUBLIC_PRODUCT_PACKAGE_ID;
+const PRODUCT_HOUSE_DATA_ID = process.env.NEXT_PUBLIC_PRODUCT_HOUSE_DATA_ID;
 
-export const PACKAGE_ID = mode == "dev" ? DEV_PACKAGE_ID : PRODUCT_PACKAGE_ID;
-export const HOUSE_DATA_ID = mode == "dev" ? DEV_HOUSE_DATA_ID : PRODUCT_HOUSE_DATA_ID;
+export const PACKAGE_ID = mode == 'dev' ? DEV_PACKAGE_ID : PRODUCT_PACKAGE_ID;
+export const HOUSE_DATA_ID = mode == 'dev' ? DEV_HOUSE_DATA_ID : PRODUCT_HOUSE_DATA_ID;
 
 // 0xdc16f545b224e66f12f2ac52688a8927209bf9640234abf91d47fd8ef8ff3ded
 // 0x72eaacc50b0158254bf44c38dac9200ab51de43eeed42a8fae2b3d65a2ac9fa3
