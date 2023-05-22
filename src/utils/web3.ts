ae59ddbe4fdde88743e910ee55e80d1328279b26
Infinite Aengus <infiniteaengus@gmail.com>
2023-05-21 23:06:15 -0400

    updated code

--- src/utils/web3.ts
+++ src/utils/web3.ts
@@ -2,8 +2,8 @@ import { devnetConnection, JsonRpcProvider } from '@mysten/sui.js';
 import { PlayResult } from '@/utils/types';
 import { mode } from '@/config';
 
-export const getPlayResultFromTx = async (digestId: string) => {
-  if (!digestId) {
+export const getPlayResultFromTx = async (txn: any) => {
+  if (!txn) {
     console.error('Invalid digestId');
   }
 
@@ -14,25 +14,14 @@ export const getPlayResultFromTx = async (digestId: string) => {
     betAmount: 0,
   };
 
-  const provider = new JsonRpcProvider(mode == "dev" ? devnetConnection : undefined);
-  // const provider = new JsonRpcProvider();
-  const txn = await provider.getTransactionBlock({
-    digest: digestId,
-    // only fetch the effects field
-    options: {
-      showEffects: true,
-      showInput: false,
-      showEvents: false,
-      showObjectChanges: false,
-      showBalanceChanges: false,
-    },
-  });
+  const provider = new JsonRpcProvider(mode == 'dev' ? devnetConnection : undefined);
   let newObjects = txn.effects?.created || [];
   for (let object of newObjects) {
     let objectContents = await provider.getObject({
       id: object.reference.objectId,
       options: { showContent: true },
     });
+    console.log(objectContents);
     if (!objectContents.error) {
       let content: any = objectContents.data?.content;
       if (content?.type.includes('Outcome')) {
