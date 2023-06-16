fbac53b4d5f87375684ad65baee7b6d90b96fdfe
InfiniteAengus <infiniteaengus@gmail.com>
2023-06-15 22:04:15 -0400

    .

--- src/components/Button/ConnectButton.tsx
+++ src/components/Button/ConnectButton.tsx
@@ -1,18 +1,27 @@
-import { ConnectButton as SuiConnectButton } from '@suiet/wallet-kit';
+import dynamic from 'next/dynamic';
+
+const SuiConnectButton = dynamic(
+  () => import('@suiet/wallet-kit').then((mod) => mod.ConnectButton),
+  { ssr: false },
+);
 
 const ConnectButton = () => {
   return (
-    <SuiConnectButton className='!relative !w-auto !rounded-[40px] !bg-[#2E4D7D] !p-8 !font-[Anja] !font-medium !uppercase !leading-none !text-white'>
-      <span className='relative z-[2]'>
-        <span className='text-[90px]'>
-          connect <br />
-        </span>
-        <span className='text-[100px]'>wallet</span>
-      </span>
-      <div className='absolute left-0 top-0 z-[1] h-full w-full !rounded-[40px] !border-8 !border-solid !border-[#3283b9] !bg-[#2E4D7D]'></div>
-      <div className='absolute -bottom-6 left-0 z-[0] h-full w-full rounded-[40px] bg-[#32678A] ' />
-      <div className='absolute left-0 top-0 z-[0] h-full w-full rounded-[40px] bg-[#5486D1] blur-[80px]' />
-    </SuiConnectButton>
+    <>
+      {typeof window !== 'undefined' && (
+        <SuiConnectButton className='!relative !w-auto !rounded-[40px] !bg-[#2E4D7D] !p-8 !font-[Anja] !font-medium !uppercase !leading-none !text-white'>
+          <span className='relative z-[2]'>
+            <span className='text-[90px]'>
+              connect <br />
+            </span>
+            <span className='text-[100px]'>wallet</span>
+          </span>
+          <div className='absolute left-0 top-0 z-[1] h-full w-full !rounded-[40px] !border-8 !border-solid !border-[#3283b9] !bg-[#2E4D7D]'></div>
+          <div className='absolute -bottom-6 left-0 z-[0] h-full w-full rounded-[40px] bg-[#32678A] ' />
+          <div className='absolute left-0 top-0 z-[0] h-full w-full rounded-[40px] bg-[#5486D1] blur-[80px]' />
+        </SuiConnectButton>
+      )}
+    </>
   );
 };
 
