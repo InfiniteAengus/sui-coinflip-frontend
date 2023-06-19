8dfcb67250c5dc2673f0c7fa9e3af018e51088e4
InfiniteAengus <InfiniteAengus@gmail.com>
2023-06-19 18:20:26 -0400

    updated matching new database api

--- src/store/slices/history.ts
+++ src/store/slices/history.ts
@@ -5,17 +5,17 @@ import API from 'src/api';
 export const getRecentHistory = createAsyncThunk('history/getRecentHistory', async () => {
   const res = await API.getGamesRequest();
   const sortedData = res.data.games.sort((a: any, b: any) => {
-    return new Date(b.details.date_created).getTime() - new Date(a.details.date_created).getTime();
+    return new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime();
   });
   const result = sortedData.map((game: any) => ({
     id: game.gameId,
-    dateCreated: game.details.date_created,
-    dateEnded: game.details.date_ended,
-    coinSide: game.details.guess === 1 ? 'head' : 'tails',
-    result: game.details.player_won,
-    player: game.details.player,
-    balance: game.details.balance,
-    txnDigest: game.details.txn_digest,
+    dateCreated: game.createdAt,
+    dateEnded: game.updatedAt,
+    coinSide: game.guess === 1 ? 'head' : 'tails',
+    result: game.playerWon,
+    player: game.player,
+    balance: game.balance,
+    txnDigest: game.txnDigest,
   }));
   return result;
 });
