3467bf48464f87254bb9bec9118de7cd825dbfdf
InfiniteAengus <InfiniteAengus@gmail.com>
2023-07-07 12:51:48 -0400

    updated env

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -4,6 +4,7 @@ export const PACKAGE_ID = process.env.REACT_APP_PACKAGE_ID || '';
 export const HOUSE_DATA_ID = process.env.REACT_APP_HOUSE_DATA_ID || '';
 
 export const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || '';
+export const API_URL = process.env.REACT_APP_API_URL || '';
 
 export const FULL_NODE = `https://fullnode.${mode}.sui.io`;
 
