a5d0c67d685869ced9645eafbebdb3b60696185f
InfiniteAengus <InfiniteAengus@gmail.com>
2023-07-08 12:21:24 -0400

    updated full node url to public

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -6,9 +6,7 @@ export const HOUSE_DATA_ID = process.env.REACT_APP_HOUSE_DATA_ID || '';
 export const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || '';
 export const API_URL = process.env.REACT_APP_API_URL || '';
 
-export const FULL_NODE = `https://api.shinami.com/node/v1/${
-	process.env.REACT_APP_SHINAMI_KEY || ''
-}`;
+export const FULL_NODE = `https://fullnode.${mode}.sui.io`;
 
 export const CAPY_NFT_TYPE = process.env.REACT_APP_CAPY_NFT || '';
 export const BULLSHARK_NFT_TYPE = process.env.REACT_APP_BULLSHARK_NFT || '';
