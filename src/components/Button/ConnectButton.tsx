23b76a62fa658df05ac5724fed9fe52aab4f6d5a
InfiniteAengus <InfiniteAengus@gmail.com>
2023-07-24 14:43:27 -0400

    fix the letter of connect wallet

--- src/components/Button/ConnectButton.tsx
+++ src/components/Button/ConnectButton.tsx
@@ -12,12 +12,10 @@ const ConnectButton = () => {
 					setModalOpen(true);
 				}}
 			>
-				<span className='relative z-[2]'>
-					<span className='text-3xl'>
-						connect <br />
-					</span>
-					<span className='text-4xl'>wallet</span>
+				<span className='relative z-20 text-3xl'>
+					connect <br />
 				</span>
+				<span className='relative z-20 text-4xl'>wallet</span>
 				<div className='absolute left-0 top-0 z-[1] h-full w-full !rounded-3xl !border-4 !border-solid !border-[#3283b9] !bg-[#2E4D7D]'></div>
 				<div className='absolute -bottom-3 left-0 z-[0] h-full w-full !rounded-3xl bg-[#32678A] ' />
 				<div className='absolute left-0 top-0 z-[0] h-full w-full !rounded-3xl bg-[#5486D1] blur-[40px]' />
