6439c2ead4f1b3cd41a2768ef4537240e2579060
InfiniteAengus <InfiniteAengus@gmail.com>
2023-07-25 15:04:21 -0400

    updated lazy import

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { WalletKitProvider } from '@mysten/wallet-kit';
 import '@suiet/wallet-kit/style.css';
-import React, { Suspense, lazy } from 'react';
+import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 
 import './App.css';
 import Layout from './layouts';
 import './styles/global.css';
-import { lazyRetry } from './utils/lazy';
+import { lazyImport } from './utils/lazy';
 
-const Main = lazy(() => lazyRetry(() => import('./pages/main'), 'main') as any);
-const Play = lazy(() => lazyRetry(() => import('./pages/play'), 'play') as any);
+const { default: Main } = lazyImport(
+	async () => await import('./pages/main'),
+	'default'
+);
+
+const { default: Play } = lazyImport(
+	async () => await import('./pages/play'),
+	'default'
+);
 
 const App = () => {
 	return (
